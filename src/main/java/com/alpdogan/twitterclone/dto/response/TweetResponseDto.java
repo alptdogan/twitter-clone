4e69b80f4bea654de48c628bb2da93eb4a1c0a01
alptdogan <alpdogan000@gmail.com>
2023-08-19 20:55:47 +0300

    Update response dtos

--- src/main/java/com/alpdogan/twitterclone/dto/response/TweetResponseDto.java
+++ src/main/java/com/alpdogan/twitterclone/dto/response/TweetResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class TweetResponseDto {
 
+    int id;
     int userId;
     String text;
 
--- src/main/java/com/alpdogan/twitterclone/dto/response/UserResponseDto.java
+++ src/main/java/com/alpdogan/twitterclone/dto/response/UserResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class UserResponseDto {
 
+    int id;
     String username;
 
 }
