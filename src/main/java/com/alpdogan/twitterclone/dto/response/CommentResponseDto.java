ae877011f4ab291ef275dd75a4e3e6f5ae285c5e
alptdogan <alpdogan000@gmail.com>
2023-08-20 11:53:04 +0300

    Create dtos to return likes and comments

---
+++ src/main/java/com/alpdogan/twitterclone/dto/response/CommentResponseDto.java
@@ -0,0 +1,12 @@
+package com.alpdogan.twitterclone.dto.response;
+
+import lombok.Data;
+
+@Data
+public class CommentResponseDto {
+
+    int id;
+    int userId;
+    int tweetId;
+
+}
---
+++ src/main/java/com/alpdogan/twitterclone/dto/response/LikeResponseDto.java
@@ -0,0 +1,12 @@
+package com.alpdogan.twitterclone.dto.response;
+
+import lombok.Data;
+
+@Data
+public class LikeResponseDto {
+
+    int id;
+    int userId;
+    int tweetId;
+
+}
