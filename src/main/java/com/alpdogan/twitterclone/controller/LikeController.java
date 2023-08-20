5aff26cc35aa5d0bc1d530118f35d5068a245c09
alptdogan <alpdogan000@gmail.com>
2023-08-20 11:28:17 +0300

    Create controller layer to save and delete likes

---
+++ src/main/java/com/alpdogan/twitterclone/controller/LikeController.java
@@ -0,0 +1,28 @@
+package com.alpdogan.twitterclone.controller;
+
+import com.alpdogan.twitterclone.dto.request.SaveLikeRequestDto;
+import com.alpdogan.twitterclone.entity.Like;
+import com.alpdogan.twitterclone.service.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/likes")
+public class LikeController {
+
+    private LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @PostMapping("/addLikeToTweets")
+    public Like addLikeToTweets(@RequestBody SaveLikeRequestDto saveLikeRequestDto) {
+        return likeService.addLikeToTweets(saveLikeRequestDto);
+    }
+
+    @DeleteMapping("/deleteLikeById")
+    public void deleteLikeById(@PathVariable int likeId) {
+        likeService.deleteLikeById(likeId);
+    }
+
+}
